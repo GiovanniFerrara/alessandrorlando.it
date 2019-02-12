fbdff5db25a3dd5b6ce94904b51f5a55660cc968
Giovanni Ferrara <gianmarco.ferrara@gmail.com>
2019-02-12 11:26:44 +0100

    update packages

---
+++ src/cms/cms.js
@@ -0,0 +1,4 @@
+// import { InlineSelectControl, InlineSelectPreview } from 'netlify-cms-widget-inline-select';
+import CMS, { init } from 'netlify-cms'
+
+// CMS.registerWidget('inline-select', InlineSelectControl, InlineSelectPreview);
